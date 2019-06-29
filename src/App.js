eda14082836bdc83de58d08c1fb2079c65fa77b3
Kirk <kkchu791@gmail.com>
2019-06-28 18:32:51 -0700

    formik template

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import InputForm from './InputForm'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div style={{padding: "100px 300px" }} className="App">
+        <InputForm />
+      </div>
+    )
+  }
 }
 
 export default App;
